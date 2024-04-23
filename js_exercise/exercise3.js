1a05de228883f00ea615e44d657af8b9cfc8d982
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2024-04-23 16:19:28 -0500

    ejercicio 3

--- js_exercise/exercise3.js
+++ js_exercise/exercise3.js
@@ -1,38 +1,49 @@
 class Persona {
   constructor(nombre, edad) {
     this.nombre = nombre;
-    this.edad = edad;
+    this.edad = parseInt(edad); // Asegúrate de manejar NaN en la aplicación real
   }
+
   imprimirDatos() {
-    document.write(`\nLa persona ${this.nombre} tienes una edad de ${this.edad}\n`);
+    document.write(`<br><br>La persona ${this.nombre} tiene una edad de ${this.edad}<br>`);
   }
 }
 
-class Empresa{
-
-  constructor(edad){
-    this.edad = edad;
+class Empresa {
+  constructor(edadTope) {
+    this.edadTope = edadTope;
   }
 
-  edadTope(){
-    
+  esInhabilitada(persona) {
+    let inhabilitada = persona.edad > this.edadTope; 
+    return inhabilitada;
   }
 
+  contar(personas) {
+    let filtro = personas.filter(persona => this.esInhabilitada(persona)).length; 
+    return filtro;
+  }
 }
+
+let personas = [];
+
 function Datos() {
-  let nombre = prompt(`Por favor ingresa el nombre de la persona 1: `);
-  let edad = parseInt(prompt(`Por favor ingresa la edad de la persona 1: `));
-  const Persona1 = new Persona(nombre, edad);
-  Persona1.imprimirDatos();
-
-  nombre = prompt(`Por favor ingresa el nombre de la persona 2: `);
-  edad = parseInt(prompt(`Por favor ingresa la edad de la persona 2: `));
-  const Persona2 = new Persona(nombre, edad);
-  Persona2.imprimirDatos();
-
-  nombre = prompt(`Por favor ingresa el nombre de la persona 3: `);
-  edad = parseInt(prompt(`Por favor ingresa la edad de la persona 3: `));
-  const Persona3 = new Persona(nombre, edad);
-  Persona3.imprimirDatos();
+  for (let i = 1; i <= 3; i++) {
+    let nombre = prompt(`Por favor ingresa el nombre de la persona ${i}:`);
+    let edad = parseInt(prompt(`Por favor ingresa la edad de la persona ${i}:`));
+    let persona = new Persona(nombre, edad);
+    personas.push(persona);
+    persona.imprimirDatos();
+  }
 }
-Datos();
+
+function Programa() {
+  Datos(); 
+
+  const empresa = new Empresa(60); 
+  const cantidadInhabilitados = empresa.contar(personas); 
+  
+  document.write(`<p>Número de personas inhabilitadas para trabajar en la empresa: ${cantidadInhabilitados}</p>`);
+}
+
+Programa(); // Ejecuta el programa
